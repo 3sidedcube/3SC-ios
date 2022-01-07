e5d33453809264b3c52fd89568ddd6a3b769ecda
benshutt <ben@3sidedcube.com>
2022-01-07 11:11:17 +0000

    Add `send-push-notification.sh`

---
+++ send-push-notification.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Apple Reference:
+# https://developer.apple.com/documentation/usernotifications/sending_push_notifications_using_command-line_tools
+#
+
+# App bundle ID
+TOPIC="<enter-app-bundle-id>"
+
+# Device token to send to
+DEVICE_TOKEN="<enter-device-token>"
+
+# APNS host to connect to
+APNS_HOST_NAME="api.push.apple.com" # api.sandbox.push.apple.com
+
+# Path to P12 file
+P12_FILE="<path-to-p12>"
+
+# Password of P12 file
+P12_PASSWORD="<p12-password>"
+
+# Hit APNS
+curl -v \
+    --header "apns-topic: ${TOPIC}" \
+    --header "apns-push-type: alert" \
+    --cert-type P12 \
+    --cert "${P12_FILE}:${P12_PASSWORD}" \
+    --data '{"aps":{"alert":"Test"}}' \
+    --http2 \
+    https://${APNS_HOST_NAME}/3/device/${DEVICE_TOKEN}
+
