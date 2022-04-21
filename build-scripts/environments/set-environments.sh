bc366869889a67475cdf69335e60b84dad28d900
Ben Shutt <ben@3sidedcube.com>
2022-04-21 11:35:29 +0100

    Update set-environments.sh

--- build-scripts/environments/set-environments.sh
+++ build-scripts/environments/set-environments.sh
@@ -16,10 +16,10 @@ set -o nounset -o errexit -o errtrace -o pipefail
 # ============================== Custom Constants ==============================
 
 # Name of the project folder
-PROJECT_FOLDER="{projectFolder}"
+PROJECT_FOLDER="{projectFolder}" # EDIT!
 
 # API Environment Info.plist key
-API_ENVIRONMENT="{apiEnvironment}"
+API_ENVIRONMENT="{apiEnvironment}" # EDIT!
 
 # ============================== Constants ==============================
 
@@ -29,8 +29,8 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 # Root of the source files in the Xcode project
 XCODE_ROOT_PATH="${DIR}/../../${PROJECT_FOLDER}"
 
-# Get the file path of the Info.plist file relative to ${DIR}
-PLIST_PATH="${XCODE_ROOT_PATH}/Info.plist"
+# Get the file path of the Info.plist file
+INFO_PLIST_PATH="${XCODE_ROOT_PATH}/Info.plist"
 
 # File name of the GoogleService-Info.plist file
 GOOGLE_SERVICE_FILE_NAME="GoogleService-Info.plist"
@@ -51,7 +51,7 @@ fi
 
 # Set API environment
 apiEnvironment=$1
-plutil -replace "${API_ENVIRONMENT}" -string "${apiEnvironment}" "${PLIST_PATH}"
+plutil -replace "${API_ENVIRONMENT}" -string "${apiEnvironment}" "${INFO_PLIST_PATH}"
 
 # File path of the new GoogleService-Info.plist file
 newGoogleServiceFile="${GOOGLE_SERVICE_DIRECTORY}/$2"
